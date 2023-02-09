bc1f8bc1a893ff23417bd1ebcb2ba1e91fdd910e
Kiran S <codehub.kirans@gmail.com>
2023-02-09 23:58:41 +0530

    Refactored for error handling and environment variable support

--- minigrepr/src/main.rs
+++ minigrepr/src/main.rs
@@ -7,14 +7,17 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     //dbg!(&args);
 
+    //use unwrap_or_else idiom when a function returns a Result with Ok(v) on success or Err(e) on error
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
+    
     println!("Searching for \"{}\" \nIn file {}", config.query, config.file_path);
 
+    //use if let idiom if a function returns only Err(e) on error and () on success
     if let Err(e) = minigrepr::run(config) {
-        println!("Application Error: {}", e);
+        eprintln!("Application Error: {}", e);
         process::exit(1);
     }    
 }
