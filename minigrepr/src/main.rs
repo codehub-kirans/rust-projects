77304b3f460543cb3abbc2b9512a1674549a433c
Kiran S <codehub.kirans@gmail.com>
2023-02-09 19:52:43 +0530

    Refactored into binary and library crates

--- minigrepr/src/main.rs
+++ minigrepr/src/main.rs
@@ -1,44 +1,24 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use minigrepr::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     //dbg!(&args);
 
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     println!("Searching for \"{}\" \nIn file {}", config.query, config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrepr::run(config) {
         println!("Application Error: {}", e);
         process::exit(1);
     }    
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let contents = fs::read_to_string(config.file_path)?;
-    println!("With text:\n{}", contents);
 
-    Ok(())
-}
 
-struct Config {
-    query: String,
-    file_path: String
-}
 
-impl Config {
-    fn new(args: &Vec<String>) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-           return Err("not enough arguments");  
-        }
-        Ok(Config {
-         query: args[1].clone(),
-         file_path: args[2].clone()
-        })
-     }
-}
