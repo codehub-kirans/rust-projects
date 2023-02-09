a13924e777b352906ff5eb70fccbfa364b8e88bc
Kiran S <codehub.kirans@gmail.com>
2023-02-09 19:39:39 +0530

    Refactored Error handling

--- minigrepr/src/main.rs
+++ minigrepr/src/main.rs
@@ -1,17 +1,29 @@
 use std::env;
 use std::fs;
+use std::process;
+use std::error::Error;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     //dbg!(&args);
 
-    let config = Config::new(&args);
-    println!("Searching for \"{}\" in file: {}", config.query, config.file_path);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+    println!("Searching for \"{}\" \nIn file {}", config.query, config.file_path);
 
-    let contents = fs::read_to_string(config.file_path)
-        .expect("Should have been able to read the file");
+    if let Err(e) = run(config) {
+        println!("Application Error: {}", e);
+        process::exit(1);
+    }    
+}
+
+fn run(config: Config) -> Result<(), Box<dyn Error>>{
+    let contents = fs::read_to_string(config.file_path)?;
     println!("With text:\n{}", contents);
-    
+
+    Ok(())
 }
 
 struct Config {
@@ -20,13 +32,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &Vec<String>) -> Config {
+    fn new(args: &Vec<String>) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("Not enough arguments")
+           return Err("not enough arguments");  
         }
-        Config {
+        Ok(Config {
          query: args[1].clone(),
          file_path: args[2].clone()
-        }
+        })
      }
 }
