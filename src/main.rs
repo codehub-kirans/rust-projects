d3c376d2c23b3ac59eb16ab4ed9686e46139d81a
Kiran S <codehub.kirans@gmail.com>
2023-03-04 13:51:02 +0530

    Added server response handling

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     net::{TcpListener, TcpStream},
     io::{prelude::*, BufReader},
 };
@@ -15,15 +16,28 @@ fn main() {
     }
 }
 
-fn handle_connection(stream: TcpStream){
-    println!("Connection Established. HTTP Req =>");
+fn handle_connection(mut stream: TcpStream){
+    print!("Connection Established. HTTP Req => ");
 
-    let buf_reader = BufReader::new(stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let buf_reader = BufReader::new(&stream);
+    let mut http_request = buf_reader.lines();
+    let http_request_line = http_request.next().unwrap().unwrap();
 
-    println!("{:#?}", http_request);
+    let (status_line, file_name) = if http_request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK\r\n", "welcome.html")
+    } else {
+        ("HTTP/1.1 400 NOT FOUND\r\n", "error.html")
+    };
+
+    let http_request: Vec<_> = http_request
+    .map(|result| result.unwrap())
+    .take_while(|line| !line.is_empty())
+    .collect();
+    println!("{}\n{:#?}", http_request_line, http_request);
+
+    let contents = fs::read_to_string(file_name).unwrap();
+    let content_length = contents.len();
+    let response = format!("{status_line}Content-Length: {content_length}\r\n\r\n{contents}");
+    
+    stream.write_all(response.as_bytes()).unwrap();
 }
