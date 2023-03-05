d2700ebbb6702de817ad8d9613bc1e0b33f4ae1d
Kiran S <codehub.kirans@gmail.com>
2023-03-05 12:39:14 +0530

    Added graceful shutdown and cleanup implementation

---
+++ README.md
@@ -0,0 +1,12 @@
+# Simple Web Server
+
+A simple HTTP web server that uses a thread pool to respond asynchronously.
+
+## License
+
+minigrepr is currently licensed under the terms of both the MIT license and the
+Apache License (Version 2.0).
+
+Thanks,  
+Kiran S  
+https://www.kirans.in  
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,41 @@
-use std::{
-    thread, 
-    sync::{mpsc, Mutex, Arc}};
+use std::{ thread, sync::{ mpsc, Mutex, Arc } };
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 struct Worker {
-    id: usize,
-    thread: thread::JoinHandle<()>,
+    _id: usize,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
     pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         println!("Creating Worker {id}.");
+        
+        // create a thread that continuously loops checking the channel for jobs 
+        let thread = thread::spawn(move || loop {
+            let msg = receiver.lock().unwrap().recv();
+            match msg {
+                Ok(job) => {
+                    println!("Worker {id} receiver job; executing...");
+                    job();
+                },
+                Err(e) => {
+                    println!("Worker {id} received disconnection request. Shutting down");
+                    break;
+                }
+            }
+
+            println!("Worker {id} completed execution.");
+        });
+
         Worker {
-            id,
-            thread: thread::spawn(move || {
-                let job = receiver.lock().unwrap().recv().unwrap();
-                println!("Worker {id} receiver job; executing...");
-                job();
-                println!("Worker {id} completed execution.");
-            }),
+            _id: id,
+            thread: Some(thread),
         }
     }
 }
 pub struct ThreadPool {
     worker_threads: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 impl ThreadPool {
@@ -37,28 +48,40 @@ impl ThreadPool {
     /// The `new` function will panic if the size is zero.
     /// Todo: return Result<ThreadPool, PoolCreationError>
     pub fn new(size: usize) -> ThreadPool {
-        
         assert!(size > 0);
 
+        // create a mpsc channel to send closure function implmenting work to be executed by spawned thread
         let (sender, receiver) = mpsc::channel();
-        
+
         //wrap receiver in Arc<Mutex<T>> to share between threads since mpsc is multiple producer single consumer
         let receiver = Arc::new(Mutex::new(receiver));
 
         println!("Setting up {size} workers...");
-        let mut worker_threads = Vec::with_capacity(size);        
+        let mut worker_threads = Vec::with_capacity(size);
         for id in 0..size {
-            worker_threads.push(Worker::new(id+1, Arc::clone(&receiver)));//create threads
+            worker_threads.push(Worker::new(id + 1, Arc::clone(&receiver))); //create threads
         }
-        ThreadPool {worker_threads, sender}
+        ThreadPool { worker_threads, sender: Some(sender) }
     }
 
-    pub fn execute<T>(&self, f: T) 
-    where
-        T: FnOnce() + Send + 'static
-    {
+    pub fn execute<T>(&self, f: T) where T: FnOnce() + Send + 'static {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.as_ref().unwrap().send(job).unwrap();
     }
-    
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+
+        //close the channel by dropping the sender end 
+        //so that the receiver end receives error to exit the worker thread's loop
+        drop(self.sender.take());
+
+        //shut down worker threads by calling join.
+        for worker in &mut self.worker_threads {
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
 }
